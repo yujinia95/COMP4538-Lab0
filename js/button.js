9f53d0e2948495edfc335fff55726978417d4edd
Yujin Jeong <te0209494@gmail.com>
2025-09-06 20:15:03 -0700

    Updating Button class

--- js/button.js
+++ js/button.js
@@ -1,3 +1,100 @@
+/**
+ * @file js/button.js
+ * @author Yujin Jeong
+ * @author Claude
+ * @version 1.0
+ * @description This file contains the Button class that holds the properties of buttons.
+ */
+
 class Button {
-    constructor()
+
+    /**
+     * Constructor for Button class.
+     * 
+     * @param {*} colour    Colour of the button
+     * @param {*} order     Order of the button
+     * @param {*} gameBoard Game board where the button is placed
+     */
+    constructor(colour, order, gameBoard) {
+        this.colour       = colour;
+        this.order        = order;
+        this.gameBoard    = gameBoard;
+        this.isClickable  = false;
+
+
+        this.btnComponent = this.createButtonComponents();
+    }
+
+    /**
+     * Create a button with components(Colour, order, size, position).
+     */
+    createButtonComponents() {
+        const button                 = document.createElement("button");
+
+        button.style.backgroundColor = this.colour;
+        button.textContent           = this.order;
+        button.className             = "game-button disable-click-button";
+
+        return button;
+    }
+
+    /**
+     * Make the button clickable.
+     * Current css for 'disable-click-button', pointer events is none.
+     */
+    makeButtonClickable() {
+        this.isClickable                = true;
+        this.btnComponent.style.cursor  = 'pointer';
+        this.btnComponent.classList.remove('disable-click-button');
+    }
+
+    /**
+     * Make the button unclickable.
+     * Current css for 'disable-click-button', pointer events is none.
+     */
+    makeButtonUnclickable() {
+        this.isClickable                = false;
+        this.btnComponent.classList.add('disable-click-button');
+    }
+
+    /**
+     * Display the number on the button.
+     */
+    displayNumbOnButton() {
+        this.btnComponent.textContent = this.order;
+    }
+
+    /**
+     * Hide the number on the button.
+     */
+    hideNumbOnButton() {
+        this.btnComponent.textContent = "";
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- js/ui-message.js
+++ js/ui-message.js
@@ -1,24 +1,37 @@
 /**
  * @file js/ui-message.js
- * @author Jeong Lab
+ * @author Yujin Jeong
  * @version 1.0
- * @description This file contains the UIMessage class that manages the user interface messages for the game.
+ * @description This file contains the UImessage class that manages the messages for the user interface (used for HTML file).
  */
 class UIMessage {
+
+    /**
+     * Constructor for UIMessage class.
+     * 
+     * @param {*} gameTitle Name of the game for head
+     * @param {*} header Name of the game for h1
+     * @param {*} numInputLabel Label for number input button
+     * @param {*} startBtn Game start button
+     * @param {*} gameMsg Game message display
+     */
     constructor(
         gameTitle       = "game-title",
         header          = "game-header",
         numInputLabel   = "num-btn-label",
-        startBtn        = "start-btn"
+        startBtn        = "start-btn",
+        gameMsg         = "game-message"
     ) {
         this.gameTitle      = document.getElementById(gameTitle);
         this.header         = document.getElementById(header);
         this.numInputLabel  = document.getElementById(numInputLabel);
         this.startBtn       = document.getElementById(startBtn);
+        this.gameMsg        = document.getElementById(gameMsg);
         
         //! Make sure I know why I need this.setTests(); here
         this.setTexts();
     }
+    
     //! Make sure I know what setTexts() does
     setTexts() {
         this.gameTitle.textContent      = GAME_MSG_FOR_USER.GAME_TITLE;
@@ -26,7 +39,26 @@ class UIMessage {
         this.numInputLabel.textContent  = GAME_MSG_FOR_USER.BTN_NUM_LABEL;
         this.startBtn.textContent       = GAME_MSG_FOR_USER.START_BTN;
     }
+
+    /**
+     * Clear the game message display.
+     */
+    clearGameMessage() {
+        if (this.gameMsg) {
+            this.gameMsg.textContent = "";
+        }
+    }
+
+    /**
+     * Display game messages "Excellent memory" or "Wrong order!" based on the provided index.
+     * 
+     * @param {*} gameMsgIndices Index to determine which message to display
+     */
+    displayGameMessage(gameMsgIndices) {
+        this.gameMsg.textContent = GAME_MSG_FOR_USER[gameMsgIndices];
+    }
 }
 
-//! Make sure I know what this does
+
+//! Make sure I move this to game manager.
 window.addEventListener("DOMContentLoaded", () => new UIMessage());
